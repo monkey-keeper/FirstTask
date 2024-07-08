43cda612f4ee3266868ccf8438565d91c23e5b4d
Ekaterina Kondratyeva <ekond202@gmail.com>
2024-07-08 23:13:05 +0000

    Revert "Merge branch 'homework-1-misha' into 'master'"
    This reverts merge request !2

--- src/main/java/gigabank/accountmanagement/entity/User.java
+++ src/main/java/gigabank/accountmanagement/entity/User.java
@@ -1,8 +1,6 @@
 package gigabank.accountmanagement.entity;
 
 import lombok.Data;
-import lombok.NonNull;
-import lombok.ToString;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,16 +10,10 @@ import java.util.List;
  */
 @Data
 public class User {
-    @NonNull
     private String id;
-    @NonNull
     private String firstName;
-    @NonNull
     private String middleName;
-    @NonNull
     private String lastName;
-    @NonNull
     private LocalDate birthDate;
-    @NonNull
     private List<BankAccount> bankAccounts;
 }

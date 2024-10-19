d0b39ecb7fcc34aea9a7a77c31e036819dcde08c
Kirill Krivonogov <hmogig@gmail.com>
2024-10-19 17:34:57 +0300

    Add analytics

--- src/main/java/gigabank/accountmanagement/repository/TransactionRepository.java
+++ src/main/java/gigabank/accountmanagement/repository/TransactionRepository.java
@@ -7,13 +7,41 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    @Query("SELECT t FROM Transaction t WHERE (:category IS NULL OR t.category = :category) AND (:transactionType IS NULL OR t.type = :transactionType)")
+    @Query("SELECT t FROM Transaction t " +
+            "WHERE (:category IS NULL OR t.category = :category) " +
+            "AND (:transactionType IS NULL OR t.type = :transactionType)")
     List<Transaction> findByCategoryAndType(@Param("category") String category,
                                             @Param("transactionType") TransactionType transactionType);
-    List<Transaction> findByCategory(String category);
+
+    @Query("SELECT t FROM Transaction t WHERE t.value = " +
+            "(SELECT MAX(t2.value) FROM Transaction t2 WHERE t2.bankAccount.id = :bankAccountId)")
+    Transaction findLargestTransactionByBankAccount(@Param("bankAccountId") Long accountId);
+
+    @Query("SELECT t FROM Transaction t WHERE t.value = " +
+            "(SELECT MIN(t2.value) FROM Transaction t2 WHERE t2.bankAccount.id = :bankAccountId)")
+    Transaction findSmallestTransactionByBankAccount(@Param("bankAccountId") Long accountId);
+
+    @Query("SELECT t FROM Transaction t WHERE t.value = " +
+            "(SELECT AVG(t2.value) FROM Transaction t2 WHERE t2.bankAccount.id = :bankAccountId)")
+    Transaction findAverageTransactionByBankAccount(@Param("bankAccountId") Long accountId);
+
+    @Query("SELECT SUM(t.value) FROM Transaction t WHERE t.category = :category AND t.bankAccount.id = :bankAccountId")
+    BigDecimal findSumTransactionByCategory(@Param("category") String category,
+                                            @Param("bankAccountId") Long accountId);
+
+    @Query("SELECT SUM(t.value) FROM Transaction t WHERE "
+            + "( t.createdDate >= :startDate) "
+            + "AND ( t.createdDate <= :endDate) "
+            + "AND t.bankAccount.id = :accountId")
+    BigDecimal findSumByDateRange(@Param("startDate") LocalDateTime startDate,
+                                  @Param("endDate") LocalDateTime endDate,
+                                  @Param("accountId") Long accountId);
+
 
 }
